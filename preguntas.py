22d60df22ed49464eeb5b52090452824248c405d
Laura  Melissa <laneirab@unal.edu.co>
2024-08-17 00:18:10 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -22,7 +22,8 @@ def pregunta_01():
     40
 
     """
-    filas = len(tbl0)
+    filas = tbl0.shape[0]
+    
     return filas
 
 
@@ -34,7 +35,7 @@ def pregunta_02():
     4
 
     """
-    columnas = len(tbl0.columns)
+    columnas = tbl0.shape[1]
     return columnas
 
 
@@ -52,7 +53,8 @@ def pregunta_03():
     Name: _c1, dtype: int64
 
     """
-    registros_columna = tbl0['_c1'].value_counts()
+    registros_columna = tbl0['_c1'].value_counts().sort_index()
+
     return registros_columna 
 
 
@@ -68,7 +70,8 @@ def pregunta_04():
     E    4.785714
     Name: _c2, dtype: float64
     """
-    promedio_c2 = tbl0.groupby('_c1').mean()['_c2']
+    promedio_c2 = tbl0.groupby('_c1')['_c2'].mean
+ 
     return promedio_c2
 
 
@@ -86,7 +89,8 @@ def pregunta_05():
     E    9
     Name: _c2, dtype: int64
     """
-    maximo_c2 = tbl0.groupby('_c1').max()['_c2']
+    maximo_c2 = tbl0.groupby('_c1')['_c2'].max()
+
     return maximo_c2
 
 
@@ -99,7 +103,8 @@ def pregunta_06():
     ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
     """
-    orden_columnas_c4 = tbl1['_c4'].unique()
+    orden_columnas_c4 = sorted(tbl1['_c4'].str.upper().unique())
+    
     return orden_columnas_c4
 
 
@@ -116,7 +121,8 @@ def pregunta_07():
     E    67
     Name: _c2, dtype: int64
     """
-    suma_c2 = tbl0.groupy('_c1').sum()['_c2']
+    suma_c2 = tbl0.groupy('_c1')['_c2'].sum()
+
     return suma_c2
 
 
@@ -136,7 +142,6 @@ def pregunta_08():
 
     """
     tbl0['suma'] = tbl0['_c0'] + tbl0['_c2']
-
     return tbl0
 
 
@@ -156,7 +161,6 @@ def pregunta_09():
 
     """
     tbl0['year'] = pd.to_datetime(tbl0['_c3']).dt.year
-
     return tbl0
 
 
@@ -174,7 +178,7 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    tabla_c1_c2 = tbl0.groupby('_c1').apply(lambda x: ':'.join(x['_c2'].astype(str)))
+    tabla_c1_c2 = tbl0.groupby('_c1')['_c2'].apply(lambda x: ':'.join(sorted(x.astype(str)))).reset_index().set_index('_c1')
     return tabla_c1_c2
 
 
@@ -194,7 +198,7 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    tabla_c0_c4 = tbl1.groupby('_c0').apply(lambda x: ','.join(x['_c4']))
+    tabla_c0_c4 = tbl1.groupby('_c0')('_c4').apply(lambda x: ','.join(sorted(x))).reset_index()
     return tabla_c0_c4
 
 
@@ -214,6 +218,7 @@ def pregunta_12():
     39   39                    ggg:3,hhh:8,jjj:5
     """
     tabla_c0_c5 = tbl2.groupby('_c0').apply(lambda x: ','.join(x['_c5a'] + ':' + x['_c5b'].astype(str)))
+
     return tabla_c0_c5
 
 
